888db5450c38dc0a81b5b6c97caf7a0d49751ad9
calren <calren24@gmail.com>
2015-10-10 09:19:29 -0700

    Adding person class

---
+++ src/person/Monday.java
@@ -0,0 +1,15 @@
+package person;
+
+public class Monday {
+
+	// create a new person with a name, the person should have 100 dollars
+
+	// make the person buy a hamburger
+
+	// make the person go to the movies
+
+	// make the person wash a car for some more money
+
+	// make the person buy a basketball
+
+}
