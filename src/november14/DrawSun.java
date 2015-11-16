f85db023f552042aa53861b4697a793689dfad48
calren <calren24@gmail.com>
2015-11-15 21:17:31 -0800

    More colors for sun

--- src/november14/DrawSun.java
+++ src/november14/DrawSun.java
@@ -15,13 +15,17 @@ public class DrawSun {
 		for (int i = 0; i < 360; i++) {
 			if (i % 2 == 0) {
 				pencil.setColor(Color.RED);
-			} else {
+			} else if (i % 3 == 0) {
 				pencil.setColor(Color.BLUE);
+			} else if (i % 5 == 0) {
+				pencil.setColor(Color.GREEN);
+			} else {
+				pencil.setColor(Color.YELLOW);
 			}
 
 			pencil.forward(300);
 			pencil.backward(300);
-			pencil.turnLeft(1);
+			pencil.turnLeft(2);
 		}
 
 	}
