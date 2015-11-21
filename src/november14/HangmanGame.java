b17a77ed36d393b5e32aa85012adb29e534864f7
calren <calren24@gmail.com>
2015-11-21 00:06:59 -0800

    Updating hang man for arrays

--- src/november14/HangmanGame.java
+++ src/november14/HangmanGame.java
@@ -6,12 +6,16 @@ public class HangmanGame {
 
 	public static void main(String[] args) {
 		int lives = 5;
-		String wordToGuess = "caren";
-		String wordToShowUser = "_____";
+		String[] wordToGuess = args;
+		String[] wordToShowUser = new String[args.length];
 		Scanner input = new Scanner(System.in);
 
+		for (int i = 0; i < args.length; i++) {
+			wordToShowUser[i] = "*";
+		}
+
 		while (lives > 0) {
-			if (!wordToShowUser.contains("_")) {
+			if (!contains(wordToShowUser, "*")) {
 				System.out.println("YOU GUESSED IT!");
 				return;
 			}
@@ -20,32 +24,43 @@ public class HangmanGame {
 			System.out.println("You have " + lives + " lives left");
 
 			// print out _ for each letter in the word
-			System.out.println(wordToShowUser);
+			System.out.println(getString(wordToShowUser));
 
 			// ask the user to guess a letter
 			System.out.println("Guess a letter");
 			String letterGuessed = input.next();
 
 			// if the letter is not in the string, subtract a life
-			if (!wordToGuess.contains(letterGuessed)) {
+			if (!contains(wordToGuess, letterGuessed)) {
 				lives = lives - 1;
 			} else {
-				String newStringToShowUser = "";
-				for (int i = 0; i < wordToGuess.length(); i++) {
-					if (Character.isLetter(wordToShowUser.charAt(i))) {
-						newStringToShowUser = newStringToShowUser + wordToShowUser.charAt(i);
-					} else if (String.valueOf(wordToGuess.charAt(i)).equals(letterGuessed)) {
-						newStringToShowUser = newStringToShowUser + letterGuessed;
-					} else {
-						newStringToShowUser = newStringToShowUser + "_";
+				for (int i = 0; i < wordToGuess.length; i++) {
+					if (wordToGuess[i].equals(letterGuessed)) {
+						wordToShowUser[i] = letterGuessed;
 					}
 				}
-				wordToShowUser = newStringToShowUser;
 			}
 		}
 
-		System.out.println("YOU LOST. THE WORD WAS " + wordToGuess);
+		System.out.println("YOU LOST. THE WORD WAS " + getString(wordToGuess));
 
+	}
+
+	public static boolean contains(String[] stringArray, String letterToCheck) {
+		for (int i = 0; i < stringArray.length; i++) {
+			if (stringArray[i].equals(letterToCheck)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static String getString(String[] stringArray) {
+		String stringToReturn = "";
+		for (int i = 0; i < stringArray.length; i++) {
+			stringToReturn += stringArray[i];
+		}
+		return stringToReturn;
 	}
 
 }
