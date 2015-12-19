4d891211070070ddff9276c116d3feaccc6093d0
calren <calren24@gmail.com>
2015-12-19 09:48:50 -0800

    Adding PresentsOrCoalTemplate

---
+++ src/preChristmas/ArrayHomework.java
@@ -0,0 +1,32 @@
+package preChristmas;
+
+public class ArrayHomework {
+
+	/*
+	 * 1) Given an array of strings, print out every other element. For example:
+	 * ["this", "is", "a", "sentence"] will print "this a" ["it's", "almost",
+	 * "christmas", "and", "it's", "getting", "cold"] will print
+	 * "its christmas its cold"
+	 *
+	 * Your method should look something like: public void
+	 * printEveryOtherWord(String[] stringArray) { }
+	 */
+
+	public void printEveryOtherWord(String[] stringArray) {
+
+	}
+
+	/*
+	 * 2) Given an array of integers, return the sum of all the numbers
+	 * divisible by 5. For example: [ 4, 6, 10, 15, 2, 5] returns 30 (10 + 15 +
+	 * 5] [ 5, 12, 3] returns 5 (just 5)
+	 *
+	 * Your method should look something like this: public int
+	 * sumOfIntegersDivisibleBy5(int[] intArray) { }
+	 */
+
+	public void sumOfIntegersDivisibleBy5(int[] intArray) {
+
+	}
+
+}
--- src/preChristmas/PresentOrCoalTemplate.java
+++ src/preChristmas/PresentOrCoalTemplate.java
@@ -3,7 +3,7 @@ package preChristmas;
 import java.util.Random;
 import java.util.Scanner;
 
-public class PresentOrCoal {
+public class PresentOrCoalTemplate {
 
 	/*
 	 * We're going to create a game for users to get a chance at getting a
@@ -28,25 +28,17 @@ public class PresentOrCoal {
 		Scanner input = new Scanner(System.in);
 
 		// initialize a String array of 20 and name it mysteryPresents
-		String[] mysteryPresents = new String[20];
 
 		// use a for loop and initialize every item in mysteryPresents to be the
 		// string COAL
-		for (int i = 0; i < 20; i++) {
-			mysteryPresents[i] = "COAL";
-		}
 
 		// random select 3 to be presents, and initialize those elements in the
 		// array to be the string "PRESENT!"
-		mysteryPresents[getRandomInt(0, 19)] = "PRESENT!";
-		mysteryPresents[getRandomInt(0, 19)] = "PRESENT!";
-		mysteryPresents[getRandomInt(0, 19)] = "PRESENT!";
 
 		// initialize a new int variable named attempts and initialize it to 3
 		// initialize a new int variable named presents and initialize it to 0
-		int attempts = 3;
-		int presents = 0;
 
+		// this is a while loop that will run while attempts > 0
 		while (attempts > 0) {
 			// ask for the user to pick a random number between 0 to 19
 			System.out.println("pick a number between 0 to 19");
@@ -54,21 +46,14 @@ public class PresentOrCoal {
 
 			// get that array element and print out the string of the array
 			// element
-			String coalOrPresent = mysteryPresents[Integer.parseInt(guess)];
 
 			// decrease attempts by 1
-			attempts--;
 			// print out what the user got
-			System.out.println("YOU GOT A : " + coalOrPresent);
 
 			// if it was PRESENT, add one to the variable presents
-			if (coalOrPresent.equals("PRESENT!")) {
-				presents++;
-			}
 		}
 
 		// when the game has ended, print out how many presents the user got
-		System.out.println("You got : " + presents + " presents");
 
 	}
 
