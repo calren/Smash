d754cbb81c44a416e93d9b3df26e4b97b1a4d2df
calren <calren24@gmail.com>
2016-01-18 21:06:23 -0800

    I hacked some code! 2d colored grid

--- src/ListAndArrayList/MagicalWordsTemplate.java
+++ src/ListAndArrayList/MagicalWordsTemplate.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MagicalWords {
+/*
+ * When completed, your program should print
+ *
+ * [snitch is magical, dumbledore is magical, hedwig is magical, snape is magical, quidditch is magical]
+
+ */
+public class MagicalWordsTemplate {
 
 	static List<String> magic = new ArrayList<>(Arrays.asList("hogwarts", "quidditch", "slytherin", "dumbledore",
 			"snape", "voldemort", "dobby", "hedwig", "firebolt", "snitch", "wand", "time keeper", "buckbeak", "fang",
@@ -14,20 +20,16 @@ public class MagicalWords {
 			"chili cheese fries", "snape", "warriors", "berkeley", "quidditch" };
 
 	public static void main(String[] args) {
-		List<String> magicalWords = new ArrayList<String>();
+		// initialize a new array list that contains strings, and name it
+		// magical words
+
 		// go through the array of randomWords and check to see if the random
 		// word is a "magical word" by checking to see if magicalWords contains
 		// it.
-		for (String s : randomWords) {
-			if (magic.contains(s)) {
-				magicalWords.add(s + " is magical");
-			}
-		}
-
 		// if it is a magical word, append the string " is magical" to the
 		// string and add it to the ArrayList awesome
 
-		System.out.println(magicalWords.toString());
+		// print out magicalWords
 	}
 
 }
