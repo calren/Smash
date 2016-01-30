bb4bb38671cd8ffb6ac1702610bf39335dbd7da7
calren <calren24@gmail.com>
2016-01-30 00:20:29 -0800

    2d arrays doc

--- src/arrays2d/exercise.java
+++ src/arrays2d/exercise.java
@@ -1,5 +1,7 @@
 package arrays2d;
 
+import java.util.ArrayList;
+
 public class exercise {
 
 	/*
@@ -9,9 +11,11 @@ public class exercise {
 	 * After initialization, the array would look something like this:
 	 * [Taylor Swift}, [Drake],         [Beyonce],       [Coldplay], [Taylor Swift]
 	 * [Blank Space],  [Hotline Bling], [Single Ladies], [Yellow],   [Love Story]
+	 * 
+	 * There's also a method called prettyPrint2DArray available for you
 	 */
 	public static void main(String[] args) {
-		String[][] songs = new String[5][5];
+		String[][] songs = new String[2][5];
 		songs[0][0] = "Taylor Swift";
 		songs[1][0] = "Blank Space";
 
@@ -26,12 +30,46 @@ public class exercise {
 
 		songs[0][4] = "Taylor Swift";
 		songs[1][4] = "Love Story";
-
-		for (int i = 0; i < songs.length; i++) {
-			if (songs[0][i] == "Taylor Swift") {
-				System.out.println(songs[1][i]);
+		
+//		prettyPrint2DArray(songs);
+		
+		// TODO add your favorite song to our 2D array
+		// follow the same format of the other songs, with the Artist and the Song Title
+		
+//		prettyPrint2DArray(songs); // this should print out the songs, now with your favorite song added
+		
+		
+		// TODO use a for loop to print out all the Taylor Swift songs
+//		prettyPrint2DArray(songs); // this should print out 'Blank Space' and 'Love Story'
+		
+		// TODO print out all the artists without any repeats
+		// So we want to print out Taylor Swift, Drake, Beyonce, Coldplay
+		// but we don't want to print out Taylor Swift twice
+		// It could be helpful to use ArrayList<String> here
+		ArrayList<String> artists = new ArrayList<String>();
+		for (int i = 0; i < songs[0].length; i++) {
+			if (!artists.contains(songs[0][i])) {
+				artists.add(songs[0][i]);
 			}
 		}
+		System.out.println(artists.toString());
+
+		
+
+//		for (int i = 0; i < songs.length; i++) {
+//			if (songs[0][i] == "Taylor Swift") {
+//				System.out.println(songs[1][i]);
+//			}
+//		}
+	}
+	
+	private static void prettyPrint2DArray(String[][] array) {
+		for(int i=0; i<array.length; i++){
+	        for(int j=0; j<array[0].length; j++){
+	            System.out.print(String.format("%20s", array[i][j]));
+	        }
+	        System.out.println("");
+	    }
 	}
 
 }
